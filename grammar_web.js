b9ba671cc9cabd51e4f2c855fb6665f804bda991
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2023-01-22 13:39:31 +0100

    start of new grammar

--- grammar_web.js
+++ grammar_web.js
@@ -16,11 +16,13 @@ module.exports = grammar({
         $.comment,
     ],
     conflicts: $ => [
-        // [$.L]
+        // [$.b, $.e],
+        [$.e],
+        // [$.o, $.de],
+        // [$.ax],
+        [$.L],
     ],
     rules: {
-        // top: $ => $.e,
-
         // module
         m: $ => seq(repeat($.i), repeat($.d)),
 
@@ -37,8 +39,9 @@ module.exports = grammar({
         sign: $ => /[+-]/,
         sym: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
-        // literals
-        L: $ => prec.left(choice(
+        // TODO: ceck prec
+        ax: $ => prec.left(seq('%', $.sym, '.', $.sym, optional(seq('.', $.sym)))),
+        L: $ => choice(
             repeat1($.dec),
             seq($.s0b, repeat1($.bin)),
             seq($.s0o, repeat1($.oct)),
@@ -53,26 +56,72 @@ module.exports = grammar({
             seq(optional($.sign), $.s0x, repeat1($.hex), $.pP, $.sign, repeat1($.dec)),
             seq(optional($.sign), $.s0x, repeat1($.hex), '.', repeat($.hex), $.pP, $.sign, repeat1($.dec)),
             seq(optional($.sign), $.s0x, repeat($.hex), '.', repeat1($.hex), $.pP, $.sign, repeat1($.dec)),
-        )),
+        ),
+        I: $ => choice(
+            seq(repeat1($.dec), repeat1($.sub)),
+            seq(repeat1($.dec), '_', repeat1($.dec)),
+        ),
+
+
+
 
         // import
         i: $ => seq('.import', $.sym, ';'),
-
-        // TODO: ceck prec
-        ax: $ => prec.left(seq('%', $.sym, '.', $.sym, optional(seq('.', $.sym)))),
-
         // declaration
         d: $ => choice(
+            seq('.ax', $.ax, ':', field('type',$.e), ';'),
             seq('.let', $.p, '=', $.e, ';'),
-        ),
+            prec(16,seq('.Pi', $.sym, optional(seq(':', field('type', $.e))), ',', field('dom',$.e), $.n)),
 
+            seq('.con', $.sym, $.p, $.n),
+            prec(15,seq('.fun', $.sym, $.p, '->', field('codom',$.e), $.n)),
+            prec(14,seq('.lam', $.sym, $.p, '->', field('codom',$.e), $.n)),
+
+            // TODO: what is the v
+            // seq('.Arr'  , $.sym, optional(seq(':', field('type', $.e))), ',', field('shape',$.e), optional(seq(',', $.v)), $.n),
+            // seq('.pack' , $.sym, optional(seq(':', field('type', $.e))), ',', field('shape',$.e), optional(seq(',', $.v)), $.n),
+            // seq('.Sigma', $.sym, optional(seq(':', field('type', $.e))), ',', field('arity',$.l), optional(seq(',', $.v)), $.n),
+            seq('.Arr'  , $.sym, optional(seq(':', field('type', $.e))), ',', field('shape',$.e), $.n),
+            seq('.pack' , $.sym, optional(seq(':', field('type', $.e))), ',', field('shape',$.e), $.n),
+            seq('.Sigma', $.sym, optional(seq(':', field('type', $.e))), ',', field('arity',$.L), $.n),
+
+            seq('.def', $.sym, $.n),
+        ),
+        // nominal definition
+        n: $ => choice(
+            ';',
+            $.o,
+        ),
+        // operand of definition
+        o: $ => choice(
+            seq('=', $.de, ';'),
+            seq('=', '{', commaSep1($.e), '}', ';'),
+        ),
         // pattern
         p: $ => choice(
             seq($.sym, optional(seq(':', $.e))),
+            seq(optional(seq($.sym, '::')), '(', commaSep1($.g), ')', optional(seq(':', $.e))),
+            seq(optional(seq($.sym, '::')), '[', commaSep1($.b), ']', optional(seq(':', $.e))),
         ),
-
+        // group pattern
+        g: $ => choice(
+            $.p,
+            // TODO: repeat1 optional is nonsensical
+            seq(repeat(seq($.sym, '::')), ':', $.e),
+        ),
+        // binder pattern
+        // TODO: precedence might be wrong solution
+        b: $ => prec(20,choice(
+            seq(optional(seq($.sym, '::')), field('type', $.e)),
+            seq(optional(seq($.sym, '::')), '[', commaSep1($.b), ']', optional(seq(':', $.e))),
+        )),
+        // optional type ascription
+        // t: $ => optional(seq(':', $.e)),
+        // optional symbol
+        // s: $ => optional(seq($.sym, '::')),
         // declaration expression
-        de: $ => (seq(repeat($.d), $.e)),
+        // TODO: ceck prec
+        de: $ => prec.right(seq(repeat($.d), $.e)),
         // expression
         e: $ => choice(
             '.Univ',
@@ -81,22 +130,16 @@ module.exports = grammar({
             '.Nat',
             '.Idx',
             '.Bool',
+            seq('.Type', $.e),
+            seq('{', $.de, '}'),
+            // TODO: optional type (else nat)
+            prec(19,seq($.L, ':', field('type', $.e))),
             '.ff',
             '.tt',
-            seq('.Type', $.e),
+            seq(choice('.bot', '.top'), optional(seq(':', field('type', $.e)))),
             $.sym,
             $.ax,
-
-            // 42: ...
-            // TODO: optional type (else nat)
-            prec.left(19,seq($.L, optional(seq(':', field('type', $.e))))),
-            seq(choice('.bot', '.top'), optional(seq(':', field('type', $.e)))),
-
-            // block
-            seq('{', $.de, '}'),
-            // e e
             prec.left(17,seq($.e, $.e)),
-
             seq($.sym, ':', field('dom', $.e), '->', field('codom', $.e), '.', field('body', $.e)),
             seq('.cn', $.sym, $.p, '=', $.de),
             prec(13,seq('.fn', $.sym, $.p, '->', field('codom', $.e), '=', $.de)),
@@ -104,27 +147,18 @@ module.exports = grammar({
             prec(12,seq('.lam'  , $.sym, $.p, '->', field('codom', $.e), '=', $.de)),
             prec.right(11,seq(field('dom', $.e), '->', field('codom', $.e))),
             // TODO: or is this the pi prec
-            // seq('Π', $.b, '->', field('codom', $.e)),
+            seq('Π', $.b, '->', field('codom', $.e)),
             // TODO: duplicate
             // seq($.e, '#', $.sym),
             prec.left(18,seq($.e, '#', field('index', $.e))),
             seq('.ins', '(', field('tuple', $.e), ',', field('index', $.e), ',', field('value', $.e), ')'),
             seq('(', commaSep1($.e), ')', optional(seq(':', field('type', $.e)))),
-            // seq('[', commaSep1($.b), ']'),
+            seq('[', commaSep1($.b), ']'),
             // TODO: what does the i mean?
             seq('‹', field('shape', $.e), ';', field('body', $.e), '›'),
             seq('«', field('shape', $.e), ';', field('body', $.e), '»'),
         ),
-
-        // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
-        comment: $ => token(choice(
-            seq('//', /(\\(.|\r?\n)|[^\\\n])*/),
-            seq(
-                '/*',
-                /[^*]*\*+([^/*][^*]*\*+)*/,
-                '/'
-            )
-        )),
+        comment: $ => token(seq('//', /.*/)),
     }
 
 });
